38ba6a99da94944a13421f5cb0af54954b638eda
Jason Chen <jchen166@hotmail.com>
2019-09-17 18:53:51 +1200

    cleaned up, working on GUI

--- src/nz/ac/vuw/ecs/swen225/a3/application/Main.java
+++ src/nz/ac/vuw/ecs/swen225/a3/application/Main.java
@@ -1,5 +1,7 @@
 package nz.ac.vuw.ecs.swen225.a3.application;
 
+import nz.ac.vuw.ecs.swen225.a3.render.MainFrame;
+
 /*PLEASE DO NOT EDIT THIS CODE*/
 /*This code was generated using the UMPLE 1.29.1.4648.92f3aa193 modeling language!*/
 
@@ -28,4 +30,7 @@ public class Main
   public void delete()
   {}
 
+  public static void main(String[] args) {
+	  new MainFrame();
+  }
 }
--- src/nz/ac/vuw/ecs/swen225/a3/render/MainFrame.java
+++ src/nz/ac/vuw/ecs/swen225/a3/render/MainFrame.java
@@ -2,8 +2,11 @@
 package nz.ac.vuw.ecs.swen225.a3.render;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 
 /**
  * MainFrame that will hold panels such as display and buttons.
@@ -23,8 +26,9 @@ public class MainFrame extends JFrame {
 	/*
 	 * Initializing the frame
 	 */
+    setMinimumSize(new Dimension(500, 500));
 	setSize(500, 500);
-	setResizable(false);
+	setResizable(true);
 	setLocationRelativeTo(null);
 	setLayout(new BorderLayout());
 	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -32,6 +36,8 @@ public class MainFrame extends JFrame {
 	/*
 	 * Adds the panels
 	 */
+	setJMenuBar(createMenuBar());
+	
 	mp = new MapPanel();
 	add(mp, BorderLayout.WEST);
 	
@@ -44,6 +50,28 @@ public class MainFrame extends JFrame {
      * @return mp
      */
     public MapPanel getMapPanel() {
-	return this.mp;
+    	return this.mp;
+    }
+    
+    /**
+     * Creates the menu bar.
+     * @return 
+     */
+    private JMenuBar createMenuBar() {
+    	JMenuBar menuBar = new JMenuBar();
+    
+    	JMenu gameMenu = new JMenu("Game");
+    	JMenuItem exitGame = new JMenuItem();
+    	
+    	JMenu optionMenu = new JMenu("Options");
+    	JMenu levelMenu = new JMenu("Level");
+    	JMenu helpMenu = new JMenu("Help");
+    	
+    	menuBar.add(gameMenu);
+    	menuBar.add(optionMenu);
+    	menuBar.add(levelMenu);
+    	menuBar.add(helpMenu);
+    	
+    	return menuBar;
     }
 }
