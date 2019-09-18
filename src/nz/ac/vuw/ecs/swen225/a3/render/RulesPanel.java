17b4c1822597a0b5160acf9d63b32a07b9e77635
Jason Chen <jchen166@hotmail.com>
2019-09-18 14:00:37 +1200

    added rules

--- src/nz/ac/vuw/ecs/swen225/a3/render/RulesPanel.java
+++ src/nz/ac/vuw/ecs/swen225/a3/render/RulesPanel.java
@@ -1,6 +1,7 @@
 package nz.ac.vuw.ecs.swen225.a3.render;
 
 import javax.swing.BorderFactory;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 
@@ -9,6 +10,8 @@ import javax.swing.border.Border;
  * game to the player.
  */
 public class RulesPanel extends JPanel {
+	private JLabel rules;
+
 	/**
 	 * Serial ID.
 	 */
@@ -21,6 +24,12 @@ public class RulesPanel extends JPanel {
 		Border innerBorder = BorderFactory.createTitledBorder("Rules");
 		Border outerBorder = BorderFactory.createEmptyBorder(5, 5, 5, 5);
 		setBorder(BorderFactory.createCompoundBorder(outerBorder, innerBorder));
+
+		rules = new JLabel();
+
+		rules.setText("Play the game.");
+
+		add(rules);
 
 		setVisible(false);
 	}
