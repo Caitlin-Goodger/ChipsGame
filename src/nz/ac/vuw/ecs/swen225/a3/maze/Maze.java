427c1e69ad8d9da29ee59c1bb97b5ff0d04097b5
Caitlin <Caitlin@DESKTOP-KKV1B0P.lan>
2019-09-22 15:43:05 +1200

    Changed where the tile data was being read in from Maze class to Map

--- src/nz/ac/vuw/ecs/swen225/a3/maze/Maze.java
+++ src/nz/ac/vuw/ecs/swen225/a3/maze/Maze.java
@@ -1,5 +1,7 @@
 package nz.ac.vuw.ecs.swen225.a3.maze;
 
+import nz.ac.vuw.ecs.swen225.a3.persistence.Map;
+
 // line 28 "model.ump"
 // line 117 "model.ump"
 public class Maze {
@@ -34,64 +36,13 @@ public class Maze {
 		this.rows = rows;
 		this.cols = cols;
 		
-		convertLevel();
+		new Map().readFile(this,"level-1");
 	}
 
 	public Tile[][] getTiles() {
 		return tiles;
 	}
 
-	/**
-	 * Converts the level with tiles as ID into a level with tiles as tile objects.
-	 */
-	public void convertLevel() {
-		// Gets the width and height of the current level.
-		int width = levelAsInt[0].length;
-		int height = levelAsInt.length;
-
-		for (int x = 0; x < width; x++) {
-			for (int y = 0; y < height; y++) {
-				int tileID = levelAsInt[x][y];
-
-				// Generic tile, given specificity in switch statement depending on id.
-				Tile tile;
-
-				switch (tileID) {
-				case 0:
-					tile = new Free(x, y, x, y);
-					break;
-				case 1:
-					tile = new Wall(x, y, x, y);
-					break;
-				case 2:
-					tile = new Chap(x, y, x, y);
-					break;
-				case 3:
-					tile = new Exit(x, y, x, y);
-					break;
-				case 4:
-					tile = new ExitLock(x, y, x, y);
-					break;
-				case 5:
-					// Replace null with key colour.
-					tile = new Key(null, x, y, x, y);
-					break;
-				case 6:
-					// Replace null with locked door colour.
-					tile = new LockedDoor(null, x, y, x, y);
-					break;
-				case 7:
-					// Replace null with info field desc.
-					tile = new InfoField(null, x, y, x, y);
-					break;
-				default:
-					tile = new Treasure(x, y, x, y);
-				}
-
-				tiles[x][y] = tile;
-			}
-		}
-	}
 
 	/**
 	 * Method for debugging, prints the converted level to see if it looks correct.
