438337cb341b9e9213d705800e2d22c4249e0360
Caitlin <Caitlin@DESKTOP-KKV1B0P.lan>
2019-09-22 15:29:50 +1200

    Added documentation comments to the XYPos class

--- src/nz/ac/vuw/ecs/swen225/a3/maze/XYPos.java
+++ src/nz/ac/vuw/ecs/swen225/a3/maze/XYPos.java
@@ -1,22 +1,36 @@
 package nz.ac.vuw.ecs.swen225.a3.maze;
 
-
-// used to keep track of the positions of Objects
 public class XYPos {
     int X;
     int Y;
-
+    
+    /**
+     * Constructor for the XYPos class. 
+     * @param x = x co-ordinate. 
+     * @param y = y co-ordinate 
+     */
     public XYPos(int x, int y){
         X = x;
         Y = y;
     }
 
-    // getter methoids
+    /**
+     * Get the x value.
+     * @return
+     */
     public int getX() { return X; }
 
+    /**
+     * Get the y value. 
+     * @return
+     */
     public int getY() { return Y; }
 
-    // used to update where the tile is when the player moves
+   /**
+    * Update the x and y positions. 
+    * @param addToX
+    * @param addToY
+    */
     public void updatePos(int addToX, int addToY){
         X = X + addToX;
         Y = Y + addToY;
