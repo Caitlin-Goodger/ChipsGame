2d901b0fba11c28a1915ccc63913aceda066d2db
Caitlin <Caitlin@DESKTOP-KKV1B0P.lan>
2019-09-22 15:17:45 +1200

    Added documentation comments to Info Field class

--- src/nz/ac/vuw/ecs/swen225/a3/maze/InfoField.java
+++ src/nz/ac/vuw/ecs/swen225/a3/maze/InfoField.java
@@ -1,39 +1,34 @@
 package nz.ac.vuw.ecs.swen225.a3.maze;
 
-/*PLEASE DO NOT EDIT THIS CODE*/
-
-/*This code was generated using the UMPLE 1.29.1.4648.92f3aa193 modeling language!*/
-
 import java.awt.image.BufferedImage;
 
-// line 54 "model.ump"
-// line 142 "model.ump"
 public class InfoField implements Tile {
 
-	// ------------------------
-	// MEMBER VARIABLES
-	// ------------------------
-
-	// InfoField Attributes
 	private String text;
 	boolean isSolid = true; // check if player can walkThrought
 	XYPos currentPosition; // keeps track of tiles position within a grid
 	XYPos currentPositionOnScreen; // keeps track of a tiles position on screen
-	BufferedImage imageToDisplay; // the image for the tokken
-	// ------------------------
-	// CONSTRUCTOR
-	// ------------------------
+	BufferedImage imageToDisplay; // the image for the token
 
+	/**
+	 * Constructor for the info field tile. 
+	 * @param aText = text for the info field. 
+	 * @param xGrid = x co-ordinate in of the tile on the grid.
+	 * @param yGrid = y co-ordinate in of the tile on the grid.
+	 * @param xScreen = x co-ordinate in of the tile on the screen.
+	 * @param yScreen = y co-ordinate in of the tile on the screen.
+	 */
 	public InfoField(String aText, int xGrid, int yGrid, int xScreen, int yScreen) {
 		text = aText;
 		currentPosition = new XYPos(xGrid, yGrid);
 		currentPositionOnScreen = new XYPos(xScreen, yScreen);
 	}
 
-	// ------------------------
-	// INTERFACE
-	// ------------------------
-
+	/**
+	 * Set the text of the info Field. 
+	 * @param aText
+	 * @return
+	 */
 	public boolean setText(String aText) {
 		boolean wasSet = false;
 		text = aText;
@@ -41,71 +36,106 @@ public class InfoField implements Tile {
 		return wasSet;
 	}
 
+	/**
+	 * Get the text of the info field. 
+	 * @return String.
+	 */
 	public String getText() {
 		return text;
 	}
 
-	public void delete() {
-	}
-
+	/**
+	 * Update the position that the tile is in.
+	 * @param direction = direction. 
+	 */
 	@Override
 	public void updatePosition(char direction) {
 		if (direction == 'N') {
-			currentPositionOnScreen.updatePos(0, 1);
+			currentPosition.updatePos(0, 1);
 		} else if (direction == 'S') {
-			currentPositionOnScreen.updatePos(0, -1);
+			currentPosition.updatePos(0, -1);
 		} else if (direction == 'E') {
-			currentPositionOnScreen.updatePos(1, 0);
+			currentPosition.updatePos(1, 0);
 		} else if (direction == 'W') {
-			currentPositionOnScreen.updatePos(-1, 0);
+			currentPosition.updatePos(-1, 0);
 		}
 	}
 
+	/**
+	 * Load the image of the tile. 
+	 */
 	@Override
 	public void loadImage() {
 
 	}
 
+	/**
+	 * Get the current position of the tile on the grid. 
+	 */
 	@Override
 	public XYPos getTilePosition() {
 		return currentPosition;
 	}
 
+	/**
+	 * Get the current position of the tile on the screen. 
+	 */
 	@Override
 	public XYPos getCurrentPositionOnScreen() {
 		return currentPositionOnScreen;
 	}
 
+	/**
+	 * Check whether the object is solid. 
+	 */
 	@Override
 	public boolean isObjectSolid() {
-		return false;
+		return isSolid;
 	}
 
+	/**
+	 * Get the path of the image. 
+	 */
 	@Override
 	public String getImagePath() {
-		return "src\\nz\\ac\\vuw\\ecs\\swen225\\a3\\IMG\\CC8.png";
+		return "src\\nz\\ac\\vuw\\ecs\\swen225\\a3\\IMG\\CC6.png";
 	}
 
+	/**
+	 * Get the y co-ordinate of the tile on the screen. 
+	 */
 	@Override
 	public int getYPositionOnScreen() {
 		return currentPositionOnScreen.getY();
 	}
 
+	/**
+	 * Get the x co-ordinate of the tile on the screen. 
+	 */
 	@Override
 	public int getXPositionOnScreen() {
 		return currentPositionOnScreen.getX();
 	}
 
+	/**
+	 * Get the y co-ordinate of the tile on the grid. 
+	 */
 	@Override
 	public int getYPosition() {
 		return currentPosition.getY();
 	}
 
+	/**
+	 * Get the x co-ordinate of the tile on the grid.
+	 */
 	@Override
 	public int getXPosition() {
 		return currentPosition.getX();
 	}
 
+	/**
+	 * To String Method.
+	 */
 	@Override
 	public String toString() {
 		return "I";
