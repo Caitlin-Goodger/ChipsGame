d2c220c224c93c735cdf0c20437ad2df766ad36d
Caitlin <Caitlin@DESKTOP-KKV1B0P.lan>
2019-09-22 15:10:11 +1200

    Added documentation comments to Chap class

--- src/nz/ac/vuw/ecs/swen225/a3/maze/Chap.java
+++ src/nz/ac/vuw/ecs/swen225/a3/maze/Chap.java
@@ -4,24 +4,23 @@ import java.awt.image.BufferedImage;
 import java.util.HashMap;
 import java.util.Map;
 
-// line 71 "model.ump"
-// line 162 "model.ump"
 public class Chap implements Tile {
 
-	// ------------------------
-	// MEMBER VARIABLES
-	// ------------------------
 	boolean isSolid = false;
 	XYPos currentPosition; // keeps track of tiles position within a grid
 	XYPos currentPositionOnScreen; // keeps track of a tiles position on screen
-	BufferedImage imageToDisplay; // the image for the tokken
+	BufferedImage imageToDisplay; // the image for the token
 
 	private Map<String, String> inventory;
-	private Tile onTile; //chap initially starts on a "free" tile
-	// ------------------------
-	// CONSTRUCTOR
-	// ------------------------
-
+	private Tile onTile; 
+	
+	/**
+	 * Constructor for the Chap file. 
+	 * @param xGrid = x co-ordinate in of the tile on the grid.
+	 * @param yGrid = y co-ordinate in of the tile on the grid.
+	 * @param xScreen = x co-ordinate in of the tile on the screen.
+	 * @param yScreen = y co-ordinate in of the tile on the screen.
+	 */
 	public Chap(int xGrid, int yGrid, int xScreen, int yScreen) {
 		currentPosition = new XYPos(xGrid, yGrid);
 		currentPositionOnScreen = new XYPos(xScreen, yScreen);
@@ -29,16 +28,9 @@ public class Chap implements Tile {
 		onTile = new Free(xGrid, yGrid, xScreen, yScreen);
 	}
 
-	// ------------------------
-	// INTERFACE
-	// ------------------------
 
-	public void delete() {
-	}
-
-	/*
-	 * given a locked door, see if chap can unlock it
-	 * 
+	/**
+	 * Given a locked door, see if chap can unlock it
 	 * @param - the door to be unlocked
 	 */
 	public boolean canUnlock(LockedDoor door) {
@@ -53,9 +45,8 @@ public class Chap implements Tile {
 		return false;
 	}
 
-	/*
-	 * pick up an item from the maze and add to chap's inventory
-	 * 
+	/**
+	 * Pick up an item from the maze and add to chap's inventory.
 	 * @param - the Tile with a pickup-able item
 	 */
 	public void pickupItem(Tile item) {
@@ -63,13 +54,13 @@ public class Chap implements Tile {
 			Key key = (Key) item;
 			inventory.put(key.getColour(), "Key");
 		}
-
 		// can add more item Tile types later i.e. ice skaters
-
 	}
 
-	/*
-	 * remove an item from the inventory after it is used i.e. a key
+	/**
+	 * Remove an item from the inventory.
+	 * @param colour = colour of the item.
+	 * @param itemType = type of the item. 
 	 */
 	public void removeItem(String colour, String itemType) {
 		if (inventory.containsKey(colour)) {
@@ -83,22 +74,27 @@ public class Chap implements Tile {
 
 	}
 	
-	/*
-	 * 
+	/**
+	 * Get the tile that chap is on.
+	 * @return Tile. 
 	 */
 	public Tile getOnTile() {
 		return this.onTile;
 	}
 	
-	/*
-	 * Update the tile that Chap is standing on after moving
+	/**
+	 * Update the tile that Chap is on.
+	 * @param tile = tile to move Chap to. 
 	 */
 	public void setOnTile(Tile tile) {
 		onTile = tile;
 	}
 
+	/**
+	 * Update the position that the tile is in.
+	 * @param direction = direction. 
+	 */
 	@Override
-	// Being world moves around Chap, chaps position changes on map but not screen
 	public void updatePosition(char direction) {
 		if (direction == 'N') {
 			currentPosition.updatePos(0, 1);
@@ -111,51 +107,81 @@ public class Chap implements Tile {
 		}
 	}
 
+	/**
+	 * Load the image of the tile. 
+	 */
 	@Override
 	public void loadImage() {
 
 	}
 
+	/**
+	 * Get the current position of the tile on the grid. 
+	 */
 	@Override
 	public XYPos getTilePosition() {
 		return currentPosition;
 	}
 
+	/**
+	 * Get the current position of the tile on the screen. 
+	 */
 	@Override
 	public XYPos getCurrentPositionOnScreen() {
 		return currentPositionOnScreen;
 	}
 
+	/**
+	 * Check whether the object is solid. 
+	 */
 	@Override
 	public boolean isObjectSolid() {
 		return isSolid;
 	}
 
+	/**
+	 * Get the path of the image. 
+	 */
 	@Override
 	public String getImagePath() {
 		return "src\\nz\\ac\\vuw\\ecs\\swen225\\a3\\IMG\\CC6.png";
 	}
 
+	/**
+	 * Get the y co-ordinate of the tile on the screen. 
+	 */
 	@Override
 	public int getYPositionOnScreen() {
 		return currentPositionOnScreen.getY();
 	}
 
+	/**
+	 * Get the x co-ordinate of the tile on the screen. 
+	 */
 	@Override
 	public int getXPositionOnScreen() {
 		return currentPositionOnScreen.getX();
 	}
 
+	/**
+	 * Get the y co-ordinate of the tile on the grid. 
+	 */
 	@Override
 	public int getYPosition() {
 		return currentPosition.getY();
 	}
 
+	/**
+	 * Get the x co-ordinate of the tile on the grid.
+	 */
 	@Override
 	public int getXPosition() {
 		return currentPosition.getX();
 	}
 
+	/**
+	 * To String Method.
+	 */
 	@Override
 	public String toString() {
 		return "C";
